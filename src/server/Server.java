6bdcd9e7cb7848a4656d686e4d910bf86f75928c
forasaken <mister.recursive@gmail.com>
2014-06-22 18:40:41 +0200

    start()...

--- src/server/Server.java
+++ src/server/Server.java
@@ -4,9 +4,8 @@ import game.Game;
 
 import java.net.Socket;
 import java.util.LinkedList;
-
 import message.Message;
-import message.MessageType;
+
 
 
 public class Server extends Thread{	
@@ -31,21 +30,8 @@ public class Server extends Thread{
 	
 	public void addClient(Socket socket){
 		Client cli = new Client(socket,this);
-		
-<<<<<<< HEAD
-		
-=======
 	}
 	
-	public void run(){
-		int i =0;
-		while(true){
-			
-		}
->>>>>>> ea5f5500d1f6aa57621f267bedb3eb9d87709297
-		
-	}
-		
 	public void sendAll(Message message){
 		for(Client cli : clients)
 			cli.sendMessage(message);
