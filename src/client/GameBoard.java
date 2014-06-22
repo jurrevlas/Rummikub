643a6a265ca6a5b8580d2b17a1af272cf09995ec
Germain <germain.wessely@gmx.de>
2014-06-22 18:43:45 +0200

    more board stuff

--- src/client/GameBoard.java
+++ src/client/GameBoard.java
@@ -24,13 +24,11 @@ public class GameBoard extends JPanel{
 		
 		for(game.Color c : game.Color.values()){
 			for(int i = 1; i<=13; i++){
-				tiles.put(c.toString()+i, new ImageIcon("src/images/"+Character.toLowerCase(c.toString().charAt(0))+i+".png"));
-				
-				System.out.println(c.toString()+i);
-				System.out.println("src/images/"+Character.toLowerCase(c.toString().charAt(0))+i+".png");
+				tiles.put(c.toString()+i, new ImageIcon("src/images/"+c.toString()+i+".png"));
 			}
 		}
-		
+		tiles.put("Blkj", new ImageIcon("src/images/Blkj.png"));
+		tiles.put("Redj", new ImageIcon("src/images/Redj.png"));
 		
 		setBackground(Color.black);
 		setLayout(new BorderLayout());
@@ -39,11 +37,15 @@ public class GameBoard extends JPanel{
 		JPanel lowerpanel = new JPanel(flow);
 		
 		lowerpanel.add(new JLabel(tiles.get("Yel1")));
+		lowerpanel.add(new JLabel(tiles.get("Blk2")));
+		lowerpanel.add(new JLabel(tiles.get("Redj")));
 		
 		JScrollPane sp = new JScrollPane(lowerpanel,
 				JScrollPane.VERTICAL_SCROLLBAR_NEVER,
 	            JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		
 		add(sp, BorderLayout.PAGE_END);
+		
+		
 	}
 }
