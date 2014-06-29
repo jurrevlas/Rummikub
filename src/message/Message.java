d4300869027415dbf703f533b1db58f67bd5ddf7
jamal <s48459@beuth-hochschule.de>
2014-06-29 22:51:45 +0200

    server fixes ....

--- src/message/Message.java
+++ src/message/Message.java
@@ -9,6 +9,7 @@ public class Message implements Serializable{
 	private static final long serialVersionUID = -6596264275448655301L;
 	
 	private String sender;	
+	private String message;
 	
 	public Message(String sender){
 		this.sender = sender;
