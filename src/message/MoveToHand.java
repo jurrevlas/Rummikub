f43e64f50b2683e98ad8bf4504c4cfd2d1a527df
jamal <s48459@beuth-hochschule.de>
2014-06-29 19:40:01 +0200

    added new message

---
+++ src/message/MoveToHand.java
@@ -0,0 +1,26 @@
+package message;
+
+import game.Set;
+import game.Tile;
+
+public class MoveToHand extends GameMessage {
+
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3678116430094117994L;
+	
+	private Tile tile;
+	
+	private Set from;
+	
+	public MoveToHand(String sender,Tile tile, Set from) {
+		super(sender);
+		this.tile = tile;
+		this.from = from;
+	}
+	
+
+}
